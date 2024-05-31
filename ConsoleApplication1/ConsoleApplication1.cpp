a20d335b9bdfc6e73ef7610957913dbb674e34ca
rokezo <dobusnikita@gmail.com>
2024-05-31 16:10:08 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    int arr[10][10];
+    arr[0][0] = n;
+
+    for (int i = 1; i < 10; i++) {
+        arr[i][0] = arr[i - 1][0] + 1;
+    }
+
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+void shiftArray(int arr[10][10], int shift, char direction) {
+    int tempArr[10][10];
+
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            tempArr[i][j] = arr[i][j];
+        }
+    }
+
+    switch (direction) {
+    case 'u':
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                arr[(i + shift) % 10][j] = tempArr[i][j];
+            }
+        }
+        break;
+    case 'd':
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                arr[(i + 10 - shift) % 10][j] = tempArr[i][j];
+            }
+        }
+        break;
+    case 'l':
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
